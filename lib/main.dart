b30899b1d3957862dfd93a0dd3ab7113380d93fd
Sandro Lovnički <sandro@scipack.xyz>
2019-04-28 18:41:44 +0200

    lib/: [remove] home.dart, [use] StatefulWidget and State<App>

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,59 @@
 import 'package:flutter/material.dart';
 
-import 'home.dart';
+void main() => runApp(new MaterialApp(
+  title: "WhatTODO",
+  theme: ThemeData( primarySwatch: Colors.blue, ),
+  home: App(),
+));
 
-void main() => runApp(App());
+class App extends StatefulWidget {
+  @override
+  AppState createState() => new AppState();
+}
 
-class App extends StatelessWidget {
+class AppState extends State<App> {
+
+  List<String> listContent = ["one", "two", "three"];
+  List<bool> listChecked = [false, false, false];
+
+  void changeListTileChecked(int index, bool value) {
+    setState(() {
+      listChecked[index] = value;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'WhatTODO',
-      theme: ThemeData( primarySwatch: Colors.blue, ),
-      home: Home(),
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("What TODO?"),
+          centerTitle: true,
+          leading: IconButton(
+              icon: Icon(Icons.settings),
+              onPressed: () { print("settings"); }
+          ),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () { print("search..."); },
+            )
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () { print("Add"); }
+        ),
+        body: ListView.builder(
+            itemCount: listContent.length,
+            itemBuilder: (BuildContext context, int index) {
+              return ListTile(
+                leading: Checkbox(value: listChecked[index], onChanged: (bool value) { changeListTileChecked(index, value); }),
+                title: Text(listContent[index]),
+                onLongPress: () { changeListTileChecked(index, !listChecked[index]); },
+                onTap: () { print("this is a tap"); },
+              );
+            }
+        )
     );
   }
 }
